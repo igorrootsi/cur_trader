ea64291addba1f651ee0ccb932edc62e66a42dc0
Igor Rootsi <igor.rootsi@gmail.com>
2017-12-01 19:17:34 +0200

    add saved prediction request drop-down

--- app/controllers/traders_controller.rb
+++ app/controllers/traders_controller.rb
@@ -1,12 +1,24 @@
+# @attr [User] current_user
 class TradersController < ApplicationController
   def show
     @req = PredictionRequest.new({})
 
     @supported_currencies = PredictionRequest.supported_currencies
+
+    @saved_requests = current_user.prediction_requests
   end
 
   def create
     @supported_currencies = PredictionRequest.supported_currencies
+
+    if params[:commit] == 'Find and save'
+      @req = current_user
+             .prediction_requests
+             .create!(prediction_request_params)
+    else
+      @req = PredictionRequest.new(prediction_request_params)
+      @req.validate
+    end
 
     @req = PredictionRequest.new(prediction_request_params)
     @req.validate
